af710b254daabac1c8fb5a29a0b55e951ee2b7c1
Kusanagi8200 <elijahkaminski230@gmail.com>
2024-08-13 22:33:21 +0200

    Update

--- update-script-fast.eng.sh
+++ update-script-fast.eng.sh
@@ -7,66 +7,66 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 echo #
-echo -e "\033[43;30m LINUX SYSTEM CLEANUP AND UPDATE SCRIPT / https://github.com/Kusanagi8200 \033[0m"
+echo  "\033[43;30m LINUX SYSTEM CLEANUP AND UPDATE SCRIPT / https://github.com/Kusanagi8200 \033[0m"
 
 echo #
 if [ `whoami` != "root" ]
 then
-        echo -e "\033[5;41;30m WARNING. YOU MUST HAVE SUDO RIGHTS TO RUN THIS SCRIPT \033[0m"
+        echo  "\033[5;41;30m WARNING. YOU MUST HAVE SUDO RIGHTS TO RUN THIS SCRIPT \033[0m"
         exit 1
 fi
 
 echo #
-echo -e "\033[43;30m ---> CHECK LOG FILES \033[0m"
+echo  "\033[43;30m ---> CHECK LOG FILES \033[0m"
 
 echo #
-if [ -e /var/log/update_upgrade.log ]
+if [  /var/log/update_upgrade.log ]
 then
-    echo -e "\033[47;32m FILE \033[0m" /var/log/update_upgrade.log = "\033[47;32m OK \033[0m"
+    echo  "\033[47;32m FILE \033[0m" /var/log/update_upgrade.log = "\033[47;32m OK \033[0m"
 else
-    echo -e "\033[47;31m FILE DOESN'T EXIST \033[0m" & touch /var/log/update_upgrade.log
-    echo -e "\033[41;33m --> FILE CREATION \033[0m" /var/log/update_upgrade.log = "\033[47;32m DONE \033[0m"
+    echo  "\033[47;31m FILE DOESN'T EXIST \033[0m" & touch /var/log/update_upgrade.log
+    echo  "\033[41;33m --> FILE CREATION \033[0m" /var/log/update_upgrade.log = "\033[47;32m DONE \033[0m"
 fi
 
 echo #
-if [ -e /var/log/update_upgrade.err ]
+if [  /var/log/update_upgrade.err ]
 then
-    echo -e "\033[47;32m FILE \033[0m" /var/log/update_upgrade.err = "\033[47;32m OK \033[0m"
+    echo  "\033[47;32m FILE \033[0m" /var/log/update_upgrade.err = "\033[47;32m OK \033[0m"
 else
-    echo -e "\033[47;31m FILE DOESN'T EXIST \033[0m" & touch /var/log/update_upgrade.err
-    echo -e "\033[41;33m --> FILE CREATION \033[0m" /var/log/update_upgrade.err = "\033[47;32m DONE \033[0m"
+    echo  "\033[47;31m FILE DOESN'T EXIST \033[0m" & touch /var/log/update_upgrade.err
+    echo  "\033[41;33m --> FILE CREATION \033[0m" /var/log/update_upgrade.err = "\033[47;32m DONE \033[0m"
 fi
 echo #
 
 echo #
-echo -e "\033[43;30m ---> PRE-UPDATE CLEANING \033[0m"
+echo  "\033[43;30m ---> PRE-UPDATE CLEANING \033[0m"
 echo #
-echo -e "\033[44;37m APT CLEAN \033[0m"
+echo  "\033[44;37m APT CLEAN \033[0m"
 apt clean 
 echo Done
 echo #
-echo -e "\033[44;37m APT AUTOCLEAN \033[0m"
+echo  "\033[44;37m APT AUTOCLEAN \033[0m"
 apt autoclean
 echo #
-echo -e "\033[44;37m APT REMOVE \033[0m"
+echo  "\033[44;37m APT REMOVE \033[0m"
 apt remove 
 echo #
-echo -e "\033[44;37m APT AUTOREMOVE \033[0m"
+echo  "\033[44;37m APT AUTOREMOVE \033[0m"
 apt autoremove
 echo #
-echo -e "\033[44;37m APT PURGE \033[0m"
+echo  "\033[44;37m APT PURGE \033[0m"
 apt purge
 echo #
-echo -e "\033[44;37m APT CHECK \033[0m"
+echo  "\033[44;37m APT CHECK \033[0m"
 apt-get check
 echo #
-echo -e "\033[43;30m <--- END OF PRE-UPDATE CLEANING \033[0m"
+echo  "\033[43;30m <--- END OF PRE-UPDATE CLEANING \033[0m"
 
 echo #
 echo # 
 
 
-echo -e "\033[43;30m ---> UPDATING PACKAGES \033[0m"
+echo  "\033[43;30m ---> UPDATING PACKAGES \033[0m"
 apt update 
 
 apt list --upgradable
@@ -77,42 +77,42 @@ apt --fix-broken install
 
 echo # 
 
-echo -e "\033[43;30m <--- UPDATING PACKAGES \033[0m"
+echo  "\033[43;30m <--- UPDATING PACKAGES \033[0m"
 echo #
 echo # 
 
 #Séquence de nettoyage systeme post mise à jour 
 
-echo -e "\033[43;30m --->  \033[0m"
+echo  "\033[43;30m --->  \033[0m"
 echo #
-echo -e "\033[44;37m APT CLEAN \033[0m"
+echo  "\033[44;37m APT CLEAN \033[0m"
 apt clean 
 echo Done
 echo #
-echo -e "\033[44;37m APT AUTOCLEAN \033[0m"
+echo  "\033[44;37m APT AUTOCLEAN \033[0m"
 apt autoclean
 echo #
-echo -e "\033[44;37m APT REMOVE \033[0m"
+echo  "\033[44;37m APT REMOVE \033[0m"
 apt remove 
 echo #
-echo -e "\033[44;37m APT AUTOREMOVE \033[0m"
+echo  "\033[44;37m APT AUTOREMOVE \033[0m"
 apt autoremove
 echo #
-echo -e "\033[44;37m APT PURGE \033[0m"
+echo  "\033[44;37m APT PURGE \033[0m"
 apt purge
 echo #
-echo -e "\033[44;37m APT CHECK \033[0m"
+echo  "\033[44;37m APT CHECK \033[0m"
 apt-get check
 echo #
 
-echo -e "\033[43;30m ---> CACHE CLEANING \033[0m"
+echo  "\033[43;30m ---> CACHE CLEANING \033[0m"
 sync; echo 3 > /proc/sys/vm/drop_caches
-echo -e "\033[44;37m DONE \033[0m"
+echo  "\033[44;37m DONE \033[0m"
 echo # 
 
-echo -e "\033[43;30m ---> BIN CLEANING \033[0m"
+echo  "\033[43;30m ---> BIN CLEANING \033[0m"
 rm -r -f ~/.local/share/Trash/files/*
-echo -e "\033[44;37m DONE \033[0m"
+echo  "\033[44;37m DONE \033[0m"
 echo #
 
 echo  "\033[43;30m ---> NETTOYAGE DES CONFIG DE PAQUETS \033[0m"
@@ -123,13 +123,13 @@ else
 fi
 echo #
 
-echo -e "\033[43;30m <--- END OF POST-UPDATE CLEANUP \033[0m"
+echo  "\033[43;30m <--- END OF POST-UPDATE CLEANUP \033[0m"
 echo #
 
 echo # 
-echo -e "\033[43;30m ---> UPDATE ERROR LOG FILE \033[0m"
+echo  "\033[43;30m ---> UPDATE ERROR LOG FILE \033[0m"
 
-if [ -e /var/log/update_upgrade.err ] && [ /var/log/update_upgrade.err -nt /var/log/update_upgrade.err ]
+if [  /var/log/update_upgrade.err ] && [ /var/log/update_upgrade.err -nt /var/log/update_upgrade.err ]
 then
     echo  "\033[5;41;37m ATTENTION \033[0m"
     cat /var/log/update_upgrade.err
@@ -158,7 +158,7 @@ if confirm "REBOOT ?"; then
    reboot
 else
     echo #
-    echo -e "\033[44;30m ---> END OF SCRIPT <--- \033[0m"
+    echo  "\033[44;30m ---> END OF SCRIPT <--- \033[0m"
     
 fi
 echo #
